78d6467c79a4f6f8b0879628febd6fac9e71d94f
ELAKIYA2000 <elaxelakiya@gmail.com>
2025-07-24 11:12:16 +0530

    for FE

--- agent/getSummary.py
+++ agent/getSummary.py
@@ -6,8 +6,10 @@ from git import Repo
 import json
 from google.generativeai import GenerativeModel
 from summarizer import summarize_with_gemini
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/analyze-compatibility", methods=["POST"])
 def analyze_compatibility():
